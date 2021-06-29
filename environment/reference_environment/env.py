5470c410fdf8da5114869ff256fc32b145662aba
moriartyjm <53300099+moriartyjm@users.noreply.github.com>
2021-06-29 13:12:11 +0100

    Add initial, deterministic net zero environment

--- environment/reference_environment/env.py
+++ environment/reference_environment/env.py
@@ -11,22 +11,13 @@ import matplotlib.pyplot as plt
 class Parameters:
     # (Avoid sampling random variables here: they would not be resampled upon reset())
     # problem-specific parameters
-    imbalance_cost_factor_high = 50
-    imbalance_cost_factor_low = 7
-    ramp_1_max = 0.2
-    ramp_2_max = 0.5
-    ramp_1_min = -0.2
-    ramp_2_min = -0.5
-    generator_1_max = 3
-    generator_1_min = 0.5
-    generator_2_max = 2
-    generator_2_min = 0.5
-    generator_1_cost = 1
-    generator_2_cost = 5
-
-    # time parameters
-    steps_per_episode = 96
-    first_peak_time = 5
+    
+    techs = 2 # number of technologies
+    scenarios = 3 # number of strategies ('scenarios' in the IEV terminology, eg Breeze, Gale, Storm)
+    reward_types = 5 # capex first, then opex, revenue, jobs, emissions
+    steps_per_episode = 30 # number of years in the planning horizon (eg. 2021 -> 2050 = 30)
+    IEV_Rewards = np.ones((scenarios, steps_per_episode, reward_types)) # rewards for each scenario in each year in the IEV model, by reward type (capex first, then opex, revenue, ...)
+    IEV_RewardSensitivities = np.full_like(IEV_Rewards,1.01) # in any strategy, when any investment is brought forward by one year, its associated rewards must be multiplied by this factor to account for changing costs/reevenues over time
 
 
 param = Parameters()  # parameters singleton
@@ -42,39 +33,29 @@ class State:
         return self.to_observation()
 
     def initialise_state(self):
+        # basic variables
+        self.scenarioWeights = np.full(param.scenarios, 1/3) # a non-negative weight for each scenario which determines its weight in the overall strategy
+        self.scenarioYears = np.ones(param.scenarios) # how many years to advance each scenario during this environment time step (0 for no progress; 1 for normal IEV speed; 2 for double IEV speed; etc)
+
         # derived variables
-        self.generator_1_level = 1.5
-        self.generator_2_level = 0.5
-        
+        self.actions = np.concatenate((scenarioWeights,scenarioYears)) # the RL action at each time step consists of the scenario weights and years
+
         # forecast variables
-        # initialise all predictions to 2
-        self.prediction_array = np.full(param.steps_per_episode, 2)
-        # sample the time of the second peak
-        self.second_peak_time = np.random.randint(low=10, high=95)
-        # add the peak demands
-        self.prediction_array[param.first_peak_time] = 4
-        self.prediction_array[self.second_peak_time] = 8
-        
-        self.agent_prediction = self.prediction_array
 
         # time variables
         # NOTE: our convention is to update step_count at the beginning of the gym step() function
         self.step_count = -1
+        self.IEV_years = np.zeros(scenarios) # for each scenario, records the latest IEV year that has been implemented
         
         # histories
         self.observations_all = []
         self.actions_all = []
         self.rewards_all = []
-        self.agent_predictions_all = []
-        self.generator_1_levels_all = []
-        self.generator_2_levels_all = []
 
     def to_observation(self):
         observation = (
             self.step_count,
-            self.generator_1_level,
-            self.generator_2_level,
-        ) + tuple (self.agent_prediction)
+        ) # + tuple (self.agent_prediction)
         
         return observation
 
@@ -84,8 +65,9 @@ class State:
             )
         return done
 
-    def set_agent_prediction(self):
-        self.agent_prediction = self.prediction_array
+    # def set_agent_prediction(self):
+    #    self.agent_prediction = np.reshape(self.cost_predictions, self.GVA_predictions, \
+    #        self.summerDemand_predictions, self.winterDemand_predictions, -1)
         
 
 
@@ -93,107 +75,53 @@ def record(state, action, reward):
     state.observations_all.append(state.to_observation())
     state.actions_all.append(action)
     state.rewards_all.append(reward)
-    state.agent_predictions_all.append(state.agent_prediction)
-    state.generator_1_levels_all.append(state.generator_1_level)
-    state.generator_2_levels_all.append(state.generator_2_level)
+    # state.agent_predictions_all.append(state.agent_prediction)
 
-def observation_space():
-    obs_low = np.full(99, -1000, dtype=np.float32) # last 96 entries of observation are the predictions
+
+def observation_space(self):
+    obs_low = np.full_like(self.state.to_observation(), 0, dtype=np.float32)
     obs_low[0] = -1	# first entry of obervation is the timestep
-    obs_low[1] = 0.5	# min level of generator 1 
-    obs_low[2] = 0.5	# min level of generator 2
-    obs_high = np.full(99, 1000, dtype=np.float32) # last 96 entries of observation are the predictions
+    obs_high = np.full_like(self.state.to_observation(), 1000, dtype=np.float32) 
     obs_high[0] = param.steps_per_episode	# first entry of obervation is the timestep
-    obs_high[1] = 3	# max level of generator 1 
-    obs_high[2] = 2	# max level of generator 2
     result = spaces.Box(obs_low, obs_high, dtype=np.float32)
     return result
 
 
 def action_space():
-    act_low = np.array(
-        [
-            0,
-            0,
-        ],
-        dtype=np.float32,
-    )
-    act_high = np.array(
-        [
-            3,
-            2,
-        ],
-        dtype=np.float32,
-    )
+    act_low = np.zeros(2 * param.scenarios, dtype=np.float32) # scenario weights and scenario years to advance
+    act_high = np.full(2 * param.scenarios, param.steps_per_episode, dtype=np.float32)
     result = spaces.Box(act_low, act_high, dtype=np.float32)
     return result
 
 
 def apply_action(action, state):
 
-    # implement the generation levels requested by the agent 
-    state.generator_1_level = action[0]
-    state.generator_2_level = action[1]
-
-    # check the previous generation levels
-    if state.step_count == 0:
-        state.generator_1_previous = 1.5
-        state.generator_2_previous = 0.5
-    else:    
-        state.generator_1_previous = state.generator_1_levels_all[state.step_count - 1]
-        state.generator_2_previous = state.generator_2_levels_all[state.step_count - 1]
-
-    # calculate ramp rates 
-    generator_1_ramp = state.generator_1_level - state.generator_1_previous
-    generator_2_ramp = state.generator_2_level - state.generator_2_previous
-
-    # curtail the actions if they exceed the ramp rate constraints
-    if generator_1_ramp > param.ramp_1_max:
-        state.generator_1_level = state.generator_1_previous + param.ramp_1_max
-    if generator_1_ramp < param.ramp_1_min:
-        state.generator_1_level = state.generator_1_previous + param.ramp_1_min
-    if generator_2_ramp > param.ramp_2_max:
-        state.generator_2_level = state.generator_2_previous + param.ramp_2_max
-    if generator_2_ramp < param.ramp_2_min:
-        state.generator_2_level = state.generator_2_previous + param.ramp_2_min
-
-    # curtail the actions if they exceed the generator level constraints
-    if state.generator_1_level > param.generator_1_max:
-        state.generator_1_level = param.generator_1_max
-    if state.generator_1_level < param.generator_1_min:
-        state.generator_1_level = param.generator_1_min
-    if state.generator_2_level > param.generator_2_max:
-        state.generator_2_level = param.generator_2_max
-    if state.generator_2_level < param.generator_2_min:
-        state.generator_2_level = param.generator_2_min
-
-    return state
+    # calculate the rewards accruing to each scenario
+    scenarioWeights = action[:scenarios]
+    scenarioYears = action[scenarios:]
+    capex = 0 # this variable will aggregate all (rebased) capital expenditure for this time step
+    IEV_LastRewards = 0 # this variable will aggregate all other rewards for this time step (these rewrads are all assumed to be annual rates)
+    for scenario in np.arange(param.scenarios): # for each scenario
+        for IEV_year in np.arange(state.IEV_years[scenario], state.IEVyears[scenario] + scenarioYears[scenario]): # for each IEV year to be implemented this time
+            # deal with capex first
+            IEV_YearReward = param.IEV_Rewards[scenario,IEV_year,0] # get the raw capex
+            for sensitivityYear in np.arange(state.step_count, IEV_year): 
+                IEV_YearReward *= param.IEV_RewardSensitivities[scenario, sensitivityYear, 0] # apply each relevant sensitivity
+            capex += scenarioWeights[scenario] * IEV_YearReward # aggregate the weighted capex
+        # now deal with remaining rewards
+        IEV_year = state.IEVyears[scenario] + scenarioYears[scenario] # identify the last IEV year to be implemented this time
+        for rewardType in np.arange(1, param.reward_types): # for each remaining reward type (these should all represent annual rates rather than one-off charges/rewards, and by convention we apply the last rate)
+            IEV_YearRate = param.IEV_Rewards[scenario,IEV_year,rewardType] # get the raw reward rate
+            for sensitivityYear in np.arange(state.step_count, IEV_year): 
+                IEV_YearRate *= param.IEV_RewardSensitivities[scenario, sensitivityYear, rewardType] # apply each relevant sensitivity
+            IEV_LastRewards += scenarioWeights[scenario] * IEV_YearRate # aggregate the weighted reward
+    reward = capex + IEV_LastRewards
+    return state, reward
 
 
-def to_reward(state, done):
-    # imbalance = total generation level - current demand level
-    imbalance = state.generator_1_level + state.generator_2_level - state.prediction_array[state.step_count]
-    if imbalance > 0:
-        imbalance_cost = imbalance * param.imbalance_cost_factor_low
-    else:
-        imbalance_cost = -1 * imbalance * param.imbalance_cost_factor_high
-    fuel_cost = param.generator_1_cost * state.generator_1_level + param.generator_2_cost * state.generator_2_level
-    reward = 0 - imbalance_cost - fuel_cost
-    
-    # uncomment for Warmup phase:
-    # if state.step_count != 1:
-    #     reward = 0 
-    
-    # uncomment for Peak phase:
-    # if state.step_count != 5:
-    #     reward = 0
-    
-    return reward
-
-
-def update_prediction_array(prediction_array):
-    prediction_array = prediction_array + 0.1 * np.random.randn(1,len(prediction_array))[0]
-    return prediction_array
+#def update_prediction_array(prediction_array):
+    #prediction_array = prediction_array + 0.1 * np.random.randn(1,len(prediction_array))[0]
+    #return prediction_array
 
 
 def plot_episode(state, fname):
@@ -239,11 +167,6 @@ def score(state):
     return {"value1" : value1}
 
 
-# TODO
-# def fetch_prediction(prediction_array, step):
-#     prediction = prediction_array[step]
-#     return prediction
-
 
 class GymEnv(gym.Env):
     def __init__(self):
@@ -263,14 +186,13 @@ class GymEnv(gym.Env):
 
     def step(self, action):
         self.state.step_count += 1
-        self.state.prediction_array = update_prediction_array(
-            self.state.prediction_array
-        )
-        self.state = apply_action(action, self.state)
-        self.state.set_agent_prediction()
+        #self.state.prediction_array = update_prediction_array(
+        #    self.state.prediction_array
+        #)
+        self.state, reward = apply_action(action, self.state)
+        #self.state.set_agent_prediction()
         observation = self.state.to_observation()
         done = self.state.is_done()
-        reward = to_reward(self.state, done)
         record(self.state, action, reward)
         return observation, reward, done, {}
 
