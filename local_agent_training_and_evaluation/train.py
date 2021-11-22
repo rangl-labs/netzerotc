9dcf669386c9388bed8690e7931e4ac8b89f9646
moriartyjm <53300099+moriartyjm@users.noreply.github.com>
2021-11-22 21:15:51 +0000

    Tidy up directory structure and improve commenting

--- local_agent_training_and_evaluation/train.py
+++ local_agent_training_and_evaluation/train.py
@@ -9,7 +9,9 @@ env = gym.make("reference_environment_direct_deployment:rangl-nztc-v0")
 
 # Train an RL agent on the environment
 trainer = Trainer(env)
-os.chdir("./noise_sigma0.1_reward_plus_step_count_jobs_increment_80models1000episodes/")
+os.chdir("./saved_models/")
+# To resume a previously trained model:
 # trainer.train_rl(models_to_train=30, episodes_per_model=1000, last_model_number=9)
+# To train from scratch:
 trainer.train_rl(models_to_train=80, episodes_per_model=1000)
 os.chdir("../")
