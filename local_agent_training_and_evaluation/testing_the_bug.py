c353950327ed11b807f082fcad77a4be13b5b200
Lachlan Mason <lachlan@quaisr.io>
2021-11-25 13:29:40 +1100

    step: remote unused commented code

--- local_agent_training_and_evaluation/testing_the_bug.py
+++ local_agent_training_and_evaluation/testing_the_bug.py
@@ -8,30 +8,6 @@ from stable_baselines3 import PPO
 import reference_environment_direct_deployment
 
 env = gym.make("reference_environment_direct_deployment:rangl-nztc-v0")
-"""
-### Evaluate RL agents model_0 to model_39
-trained_models = 40
-episodes_per_model = 1000
-mean_rewards = []
-for i in [39]:
-    agent = PPO.load("MODEL_"+str(i))
-    evaluate = Evaluate(env, agent)
-    seeds = evaluate.read_seeds(fname="seeds.csv")
-    mean_reward = evaluate.RL_agent(seeds) # Add your agent to the Evaluate class and call it here e.g. evaluate.my_agent(seeds)
-    mean_rewards.append(mean_reward)
-print("Mean rewards for RL agent 39:", mean_rewards)
-
-mean_rewards = []
-for i in [39]:
-    agent = PPO.load("MODEL_"+str(i))
-    env = gym.make("reference_environment_direct_deployment:rangl-nztc-v0")
-    #agent.set_env(env)
-    evaluate = Evaluate(env, agent)
-    seeds = evaluate.read_seeds(fname="seeds.csv")
-    mean_reward = evaluate.RL_agent(seeds) # Add your agent to the Evaluate class and call it here e.g. evaluate.my_agent(seeds)
-    mean_rewards.append(mean_reward)
-print("Mean rewards for RL agent 39 again:", mean_rewards)
-"""
 
 list1 = []
 rewards = []
@@ -55,11 +31,8 @@ for seed in seeds:
 rewards.append(sum(env.state.rewards_all))
 mean_reward1 = np.mean(rewards)
 
-print("######################################")
-
-# del env
 del model
-# env = gym.make("reference_environment_direct_deployment:rangl-nztc-v0")
+
 model = PPO.load("MODEL_0")
 list2 = []
 rewards = []
@@ -78,9 +51,3 @@ mean_reward2 = np.mean(rewards)
 
 print("Final results from 1st run: ", mean_reward1)
 print("Final results from 2st run: ", mean_reward2)
-
-# for i in range(len(list1)):
-#     #if list1[i] != list2[i]:
-#     print(i,list1[i],list2[i])
-#
-# print(list1[-2] == list2[-2])
