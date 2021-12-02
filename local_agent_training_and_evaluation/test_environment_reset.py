a047008819741ef7677eb1be7bbf5d7139af9454
Lachlan Mason <lachmason@gmail.com>
2021-12-03 01:23:58 +1100

    step: train and test one model (#71)
    * step: train and test one model
    
    * step: commit a single model to the code base
    
    * step: remove unused imports
    
    * step: remote unused commented code
    
    * step: use meaningful file name
    
    * step: makee test executable
    
    * step: local model from saved_models folder
    
    * step: add assert statement to test
    
    * step: log the seed value to stdout
    
    * step: add comments aimed at new contributors
    
    * step: add call to env.check_reset()
    
    * step: add explicit call the env.reset()

---
+++ local_agent_training_and_evaluation/test_environment_reset.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import csv
+import numpy as np
+import gym
+from stable_baselines3 import PPO
+
+import reference_environment_direct_deployment
+
+# this test shows that we can run the environment twice and get the same mean reward
+# the environment is reset using a call to env.reset()
+
+# create the environment
+env = gym.make("reference_environment_direct_deployment:rangl-nztc-v0")
+
+# run the environment for the first time
+list1 = []
+rewards = []
+model = PPO.load("./saved_models/MODEL_0")
+seeds = []
+for row in csv.reader(open("seeds.csv")):
+    seeds.append(int(row[0]))
+seeds = seeds[:1]  # try with a single seed to make it fast
+
+for seed in seeds:
+    print(f"{seed=}")
+    list1.append(seed)
+    env.seed(seed)
+    obs = env.reset()
+    list1.append(obs)
+
+    print(f"INFO: 1st run")
+    step = -1
+    while not env.state.is_done():
+        step += 1
+        print(f"INFO: {step=}")
+        action, _states = model.predict(obs, deterministic=True)
+        obs, _, _, _ = env.step(action)
+        list1.append(obs)
+        list1.append(action)
+rewards.append(sum(env.state.rewards_all))
+mean_reward1 = np.mean(rewards)
+
+# for completeness we can delete and reload the model
+# though, this is redundant and does not change the outcome
+del model
+model = PPO.load("./saved_models/MODEL_0")
+
+# run the environment for the second time
+list2 = []
+rewards = []
+for seed in seeds:
+    print(f"{seed=}")
+    list2.append(seed)
+    env.seed(seed)
+    obs = env.reset()
+    list2.append(obs)
+
+    print(f"INFO: 2nd run")
+    step = -1
+    while not env.state.is_done():
+        step += 1
+        print(f"INFO: {step=}")
+        action, _states = model.predict(obs, deterministic=True)
+        obs, _, _, _ = env.step(action)
+        list2.append(obs)
+        list2.append(action)
+rewards.append(sum(env.state.rewards_all))
+mean_reward2 = np.mean(rewards)
+
+# check that we get the same results from both runs
+print("Final results from 1st run: ", mean_reward1)
+print("Final results from 2nd run: ", mean_reward2)
+assert np.isclose(a=[mean_reward1], b=[mean_reward2])
+
+# explicitly reset the environment, and call the check method
+env.reset()
+reset_diff = env.check_reset()
+print(f"{reset_diff=}")
--- local_agent_training_and_evaluation/train.py
+++ local_agent_training_and_evaluation/train.py
@@ -10,8 +10,11 @@ env = gym.make("reference_environment_direct_deployment:rangl-nztc-v0")
 # Train an RL agent on the environment
 trainer = Trainer(env)
 os.chdir("./saved_models/")
+
+# To train from scratch:
+trainer.train_rl(models_to_train=1, episodes_per_model=1000)
+
 # To resume a previously trained model:
 # trainer.train_rl(models_to_train=30, episodes_per_model=1000, last_model_number=9)
-# To train from scratch:
-trainer.train_rl(models_to_train=80, episodes_per_model=1000)
+
 os.chdir("../")
