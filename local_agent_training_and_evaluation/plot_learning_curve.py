8b74a8afddb9adbb727f7aa1679324c9b150b84c
Jia Chen <jia-chen@trainVM.bjpbhxw1yoxupinhcut2lnrxxe.zx.internal.cloudapp.net>
2021-12-13 11:49:50 +0000

    Minor polishing and removal

--- local_agent_training_and_evaluation/plot_learning_curve.py
+++ local_agent_training_and_evaluation/plot_learning_curve.py
@@ -20,7 +20,7 @@ mean_rewards = []
 for i in range(trained_models):
     agent = PPO.load("MODEL_"+str(i))
     evaluate = Evaluate(env, agent)
-    seeds = evaluate.read_seeds(fname="seeds.csv")
+    seeds = evaluate.read_seeds(fname="../seeds.csv")
     mean_reward = evaluate.RL_agent(seeds) # Add your agent to the Evaluate class and call it here e.g. evaluate.my_agent(seeds)
     mean_rewards.append(mean_reward)
 print("Mean rewards for RL agents:", mean_rewards)
