016432cfe794049c8f54241c1a417b1cd455108a
L. R. Mason <lmason@turing.ac.uk>
2021-12-18 07:04:01 +0000

    Update other files for the 2 phases

--- random_agent_submission/test_container.py
+++ random_agent_submission/test_container.py
@@ -60,3 +60,89 @@ for seed in seeds:
 
 print("Evaluation completed using {} seeds.".format(len(seeds)))
 print("Final average score: ", np.average(scores))
+
+scores = []
+for seed in seeds:
+    submission = client.containers.run(
+        image="submission:v0.1.0",
+        name="agent",
+        network="evalai_rangl",
+        detach=False,
+        auto_remove=True,
+        # command="sleep infinity",  # debug
+        environment={
+            "RANGL_SEED": seed,
+            "RANGL_ENVIRONMENT_URL": "http://nztc:5001",
+        },
+    )
+    logger.debug(f"Created submission")
+    logger.debug(f"Completed submission")
+
+    # TODO evaluation script should be executed here, but while prototyping we do it here
+
+    # fetch results from environment
+    output = submission.decode("utf-8").strip()
+
+    print("output")
+    print(output)
+
+    # assumption: final line in stdout is the instance id
+    instance_id = output.split("\n")[-1]
+    logger.debug(f"Instance id: {instance_id}")
+
+    # send a request to the score endpoint
+    ENVIRONMENT_URL = "http://localhost:5001/"
+
+    # fetch score for submission
+    url = f"{ENVIRONMENT_URL}/v1/envs/{instance_id}/score/"
+    response = requests.get(f"{ENVIRONMENT_URL}/v1/envs/{instance_id}/score/").json()
+
+    score = response["score"]["value1"]
+    print("score:", score)
+    scores.append(score)
+
+print("Evaluation completed using {} seeds.".format(len(seeds)))
+print("Open-loop final average score: ", np.average(scores))
+
+scores = []
+for seed in seeds:
+    submission = client.containers.run(
+        image="submission:v0.1.0",
+        name="agent",
+        network="evalai_rangl",
+        detach=False,
+        auto_remove=True,
+        # command="sleep infinity",  # debug
+        environment={
+            "RANGL_SEED": seed,
+            "RANGL_ENVIRONMENT_URL": "http://nztc:5002",
+        },
+    )
+    logger.debug(f"Created submission")
+    logger.debug(f"Completed submission")
+
+    # TODO evaluation script should be executed here, but while prototyping we do it here
+
+    # fetch results from environment
+    output = submission.decode("utf-8").strip()
+
+    print("output")
+    print(output)
+
+    # assumption: final line in stdout is the instance id
+    instance_id = output.split("\n")[-1]
+    logger.debug(f"Instance id: {instance_id}")
+
+    # send a request to the score endpoint
+    ENVIRONMENT_URL = "http://localhost:5002/"
+
+    # fetch score for submission
+    url = f"{ENVIRONMENT_URL}/v1/envs/{instance_id}/score/"
+    response = requests.get(f"{ENVIRONMENT_URL}/v1/envs/{instance_id}/score/").json()
+
+    score = response["score"]["value1"]
+    print("score:", score)
+    scores.append(score)
+
+print("Evaluation completed using {} seeds.".format(len(seeds)))
+print("Closed-loop final average score: ", np.average(scores))
