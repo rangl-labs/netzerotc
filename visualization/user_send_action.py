eb35f4bc91e5f9de9e253115f6726ac1d2837aef
Jia-Chen Hua <jia-chen.hua@qmul.ac.uk>
2022-02-14 11:17:17 +0000

    Implemented user control of 'env.step()' via sending/receiving OSC commands/messages https://github.com/rangl-labs/netzerotc/issues/116.
    Steps to test:
    (1) Open the BasicVisualization1 project in Unreal Editor, open the main level of this project, and click "Simulate" in the Play or Run sub-menu;
    (2) Open an IPython console and run user_send_action.py;
    (3) Open **another**  IPython console and run send_receive_osc_env.py;
    (4) In the **first** IPython console, input or accept agent's action.

---
+++ visualization/user_send_action.py
@@ -0,0 +1,65 @@
+#%% 
+
+#%% 
+import logging
+
+import pandas as pd
+import numpy as np
+import gym
+import argparse
+import random
+import time
+
+from pythonosc.osc_server import BlockingOSCUDPServer
+from pythonosc.udp_client import SimpleUDPClient
+
+from pythonosc.dispatcher import Dispatcher
+from typing import List, Any
+
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+
+dispatcher = Dispatcher()
+
+done = False
+current_step = -1
+agent_action = np.zeros(3)
+def set_agent_action(address: str, *args: List[Any]) -> None:
+    global done
+    global agent_action
+    global current_step
+
+    # Check that address starts with agent_action
+    if not address[:-1] == "/agent_action":  # Cut off the last character
+        return
+
+    # value1 = args[0]
+    # value2 = args[1]
+    # value3 = args[2]
+    # user_action_no = address[-1]
+    # print(f"Setting agent_action {user_action_no} values: {value1}, {value2}, {value3}")
+    set_agent_action.done = args[0]
+    set_agent_action.values = np.array([args[1], args[2], args[3]])
+    done = args[0]
+    agent_action = np.array([args[1], args[2], args[3]])
+    current_step = args[4]
+
+dispatcher.map("/agent_action*", set_agent_action)  # Map wildcard address to set_agent_action function
+
+# Specify the OSC client and server address
+client = SimpleUDPClient("127.0.0.1", 1338)
+server = BlockingOSCUDPServer(("127.0.0.1", 1337), dispatcher)
+
+user_action = agent_action
+while not done:
+    server.handle_request()
+    print("\n\nThe Storm/RL agent's action of current step %d for increments in [offshore Wind, blue Hydrogen, green Hydrogen] are: [%f, %f, %f]" %(current_step, agent_action[0], agent_action[1], agent_action[2]))
+    accept_agent_action = input("Do you want do accept this agent's action? ('n' to input manually / 'y' or any other key to accept) ")
+    if (accept_agent_action.lower() == 'n'):
+        user_action[0] = np.float64(input("Enter the increment in offshore Wind: "))
+        user_action[1] = np.float64(input("Enter the increment in blue Hydrogen: "))
+        user_action[2] = np.float64(input("Enter the increment in green Hydrogen: "))
+    else:
+        user_action = agent_action
+    client.send_message("/user_action1", user_action)
