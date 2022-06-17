fa572675e87606fe93a278193e295094ff9b4b25
Jia-Chen Hua <jia-chen.hua@qmul.ac.uk>
2022-06-17 14:17:28 +0100

    Updated the Unreal Engine project files: replaced blue wind turbines by blue offshore oil platforms to represent blue hydrogen; Documented the main python script `osc_UE_UMG_env_3FixedPolicies.py` by comments inside the script; Updated README.md.MD and uploaded a screen recording on how to run and control the UMG widgets to change scenarios and years.

--- visualization/README.md
+++ visualization/README.md
@@ -4,7 +4,7 @@
 
 This folder contains a visualization of the `env.py` in Unreal Engine.
 
-The general idea is to use some objects (e.g., wind turbines) in a virtual world with UK geography & satellite images created in Unreal Engine to represent the outputs of `env.render()`.
+The general idea is to use some objects (e.g., wind turbines and oil platforms) in a virtual world with UK geography & satellite images created in Unreal Engine to represent the outputs of `env.render()`.
 
 The `env.render()` can output some values (typically stored in a NumPy array) to be visualized, e.g., the deployment numbers of the 3 technologies, rewards, CO2 emissions, etc. Visualizing these values can provide the end-user an idea of the current status of the `env`.
 
@@ -12,11 +12,15 @@ To send the NumPy array containing these values to Unreal Engine for visualizati
 
 There is an OSC plug-in developed for Unreal Engine to receive numerical array as OSC messages sent from outside Unreal Engine, and there are several implementations of OSC in Python to send and receive OSC messages containing arrays of numeric values. Several examples can be found at https://pypi.org/search/?q=%22open+sound+control%22&o=
 
-For testing purposes, the `render()` method of `env.py` is unchanged. Instead, in the `send_receive_osc_env.py`, the `python-osc` package is used to send `step_count`, `reward`, `3-element deployments`, and `CO2 emission amount` of the `env` to Unreal Engine for visualization.
+For testing purposes, the `render()` method of `env.py` is unchanged. Instead, in the `osc_UE_UMG_env_3FixedPolicies.py` script in this folder, the `python-osc` package is used to send `step_count`, `reward`, `3-element deployments`, and `CO2 emission amount` of the `env` to Unreal Engine for visualization.
 
-The `UE_Projects` folder contains project files of Unreal Engine, and within these files, there is an OSC server built within Unreal Engine using the OSC plug-in to receive the OSC message sent from `send_receive_osc_env.py`, and according to the OSC messages received, it will change the physical properties of some objects (e.g., the number of wind turbines built) to represent the deployment numbers in the OSC messages sent from `send_receive_osc_env.py`.
+The `UE_Projects` folder contains project files of Unreal Engine, and within these files, there is an OSC server built within Unreal Engine using the OSC plug-in to receive the OSC message sent from `osc_UE_UMG_env_3FixedPolicies.py`, and according to the OSC messages received, it will change the physical properties of some objects (e.g., the number of wind turbines and oil platforms built) to represent the deployment numbers in the OSC messages sent from `osc_UE_UMG_env_3FixedPolicies.py`.
 
+<!---
 An end-user can control the `env.step()` by manually specifying an action, or simply accept the Storm or RL agent's proposed action. Moreover, the end-user can also rewind the `env.state` backward in time and restart/re-input some new actions. These are implemented in the `user_send_action.py`. The `env` will wait for end-user's input/action before `env.step(action)` or it will be rewound back to a previous step, which are implemented in `send_receive_osc_env.py`.
+-->
+
+An end-user can control the `env.step()` in Unreal Ungine's UMG Widgets by manually specifying the scenario (out of Breeze, Gale, or Storm) and the year to visualize; an example is shown in the following screen recording: ![screenrecording](https://github.com/rangl-labs/netzerotc/raw/visualization/visualization/HowToRunUE.gif "Unreal Engine screen recording"); A higher resolution version is available in the video https://github.com/rangl-labs/netzerotc/raw/visualization/visualization/HowToRunUE.mp4
 
 For more information of OSC implementations in Python, read the documentations of [python-osc](https://python-osc.readthedocs.io/en/latest/) and [osc4py3](https://osc4py3.readthedocs.io/en/latest/). The history of development can be found at https://github.com/orgs/rangl-labs/projects/1
 
@@ -26,14 +30,22 @@ To give this visualization a test:
 
 1. Install Unreal Engine at https://www.unrealengine.com/en-US/download/creators?install=true
 
+<!---
 2. Within Unreal Engine, open the `BasicVisualization1` project in `UE_Projects` folder. Click the triangle of drop-down menu next to the "Play" button, and then select "Simulate" (Please see the following screenshot ![screenshot](https://github.com/rangl-labs/netzerotc/raw/visualization/visualization/HowToRunUE.png "Unreal Engine screenshot") showing the buttons).
+-->
 
-3. Open a Python console and set current directory to this folder, then run `python user_send_action.py`.
+2. Open a Python console and set current directory to this folder, then run `python osc_UE_UMG_env_3FixedPolicies.py`.
 
+3. Within Unreal Engine, open the `BasicVisualization1` project in `UE_Projects` folder. Follow the screen recording below to run or play, and specify scenario and year to visualize (Note: a higher resolution version is available in the video https://github.com/rangl-labs/netzerotc/raw/visualization/visualization/HowToRunUE.mp4): ![screenrecording](https://github.com/rangl-labs/netzerotc/raw/visualization/visualization/HowToRunUE.gif "Unreal Engine screen recording")
+
+<!---
 4. Open **another** Python console and run `python send_receive_osc_env.py`.
 
 5. After a while, the **first** Python console will prompt some message asking an end-user to specify or accept agent's action, and then the Python console of `send_receive_osc_env.py` will take user's input and manipulate the `env` by `env.step()` or rewind `env.state` to a previous step, and then send the array of numeric values to Unreal Engine for visualization.
+--->
 
+<!---
 Note: Depending on your OS, Unreal Engine will stop the real-time rendering if the Unreal Engine's program window is not in focus or on top of the desktop, e.g., when you input some numbers in the Python console of `user_send_action.py`. Therefore, to see the visualization showing/changing in real-time, you may need to switch to Unreal Engine's program window for each step when you manually input in the Python console of `user_send_action.py`.
+-->
 
 There are some nice video tutorials of how to navigate the virtual 3D world inside Unreal Engine, both on [YouTube](https://www.youtube.com/watch?v=j2CKS6G3G2k) and [Epic Games websites](https://www.unrealengine.com/en-US/onlinelearning-courses/your-first-hour-in-unreal-engine-4) (free registration required; some people had uploaded it to [YouTube](https://www.youtube.com/watch?v=jNUaR6y7sE4)). A very nice and comprehensive course can also be found on [YouTube](https://www.youtube.com/watch?v=_a6kcSP8R1Y).
--- visualization/osc_UE_UMG_env_3FixedPolicies.py
+++ visualization/osc_UE_UMG_env_3FixedPolicies.py
@@ -24,18 +24,23 @@ from typing import List, Any
 # logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
 
+# define the event dispatcher for python-osc server; more details and explanation: https://github.com/attwad/python-osc and https://python-osc.readthedocs.io/
 dispatcher = Dispatcher()
 
-user_action = np.zeros(3)
-steps_back = 0
-user_policy = 1
-steps_to_apply = 0
-user_year = 2031
+## initialize user specified actions, steps to jump back and move forward (deprecated, as here we only visualize the 3 scenarios)
+# user_action = np.zeros(3)
+# steps_back = 0
+# steps_to_apply = 0
+user_policy = 1 # initialize the user specified scenarios/policies number: 0 for breeze, 1 for gale, 2 for storm
+user_year = 2031 # initialize the 1st year to apply action from the 3 possible scenarios, which is 2031
+
+# define the function to be associated to the dispatcher in python-osc server; more details and explanation: https://github.com/attwad/python-osc and https://python-osc.readthedocs.io/
 def set_user_action(address: str, *args: List[Any]) -> None:
-    global steps_back
-    global user_action
+    ## Set the variables below to be global, such that they can be changed by this function after receiving OSC message:
+    # global steps_back
+    # global user_action
+    # global steps_to_apply
     global user_policy
-    global steps_to_apply
     global user_year
     # We expect three float arguments
     # if not len(args) == 3 or type(args[0]) is not float or type(args[1]) or type(args[2]) is not float:
@@ -77,28 +82,30 @@ def set_user_action(address: str, *args: List[Any]) -> None:
     #     print("\n\nRewinding env.state from env.state.step_count = %d back to %d ..." %(current_step, current_step + steps_to_apply))
     print("\n\nSetting env.state to user specified year = %d, after setting user_policy number to %d ..." %(current_year, user_policy))
 
-dispatcher.map("/user_action*", set_user_action)  # Map wildcard address to set_user_action function
+dispatcher.map("/user_action*", set_user_action)  # Map wildcard address to set_user_action functionm; ore details and explanation: https://github.com/attwad/python-osc and https://python-osc.readthedocs.io/
 
 
-# Specify the OSC (in Unreal Engine) server address
+# Specify the OSC server address (hosted by the "OSCMessageRender" blueprint in Unreal Engine)
 ip = "127.0.0.1"
 port = 5005
 client_toUEosc = SimpleUDPClient(ip, port)  # Create OSC client
-client_toUMGpopup = SimpleUDPClient("127.0.0.1", 1336)
+# Create another OSC client to send to the other OSC server hosted by the "MultiWindow_SlideYearsAndSwitch3Policies_UserReceivesOSC" blueprint in Unreal Engine
+client_toUMGMultiWindow = SimpleUDPClient("127.0.0.1", 1336)
 
 # Specify the OSC client and server address
-client = SimpleUDPClient("127.0.0.1", 1337)
+# client = SimpleUDPClient("127.0.0.1", 1337) # deprecated; previously used to send user actions and step count back to Unreal Engine
+# This is the server hosted by this script to listen to OSC message sent from UMG Widget "UMG_MultiWindow_UserSlideYearsAndSwitch3PoliciesOSC" in Unreal Engine:
 server = BlockingOSCUDPServer(("127.0.0.1", 1338), dispatcher)
 
 
-# Create environments named env_breeze, env_gale, env_storm
+# Create environments
 env = gym.make("reference_environment_direct_deployment:rangl-nztc-v0")
 # env_breeze = gym.make("reference_environment_direct_deployment:rangl-nztc-v0")
 # env_gale = gym.make("reference_environment_direct_deployment:rangl-nztc-v0")
 # env_storm = gym.make("reference_environment_direct_deployment:rangl-nztc-v0")
 # set the seed equal to current time in rounded seconds
 # seed = int(time.time())
-seed = 123456
+seed = 123456 # use a fixed seed; can change to other integers
 env.seed(seed)
 # env_breeze.seed(seed)
 # env_gale.seed(seed)
@@ -115,18 +122,22 @@ done = False
 # done_Storm = False
 # rewards = []
 # deployments = np.array(np.array(pd.read_excel('STORM_Deployments_Modified.xlsx'))[-(env.param.steps_per_episode+1):,1:],dtype=np.float32)
+# Read the deployments numbers from the 3 scenarios:
 deployments_Breeze = np.array(np.array(pd.read_excel('BREEZE_Deployments_Modified.xlsx'))[-(env.param.steps_per_episode+1):,1:],dtype=np.float32)
 deployments_Gale = np.array(np.array(pd.read_excel('GALE_Deployments_Modified.xlsx'))[-(env.param.steps_per_episode+1):,1:],dtype=np.float32)
 deployments_Storm = np.array(np.array(pd.read_excel('STORM_Deployments_Modified.xlsx'))[-(env.param.steps_per_episode+1):,1:],dtype=np.float32)
 # actions = deployments[1:,:] - deployments[:-1,:]
 
+# Apply actions of Breeze, and then record the reward and emission:
 actions = deployments_Breeze[1:,:] - deployments_Breeze[:-1,:]
 while not env.state.is_done():
     observation, reward, _, _ = env.step(actions[env.state.step_count + 1])
 rewards_Breeze = np.array(env.state.rewards_all)
 emissions_Breeze = np.array(env.state.emission_amount_all)
-# env.plot("U:/Documents/Work/Research/VisualizingRangLEnvInUnrealEngine/UE_Projects/BasicVisualization1/Content/Figures/Breeze.png")
+# Plot the figure and save to Unreal Engine's project file (only needed when the random seed is changed above)
+# env.plot("./UE_Projects/BasicVisualization1/Content/Figures/Breeze.png")
 
+# Apply actions of Gale, and then record the reward and emission:
 env.seed(seed)
 env.reset()
 actions = deployments_Gale[1:,:] - deployments_Gale[:-1,:]
@@ -134,8 +145,10 @@ while not env.state.is_done():
     observation, reward, _, _ = env.step(actions[env.state.step_count + 1])
 rewards_Gale = np.array(env.state.rewards_all)
 emissions_Gale = np.array(env.state.emission_amount_all)
-# env.plot("U:/Documents/Work/Research/VisualizingRangLEnvInUnrealEngine/UE_Projects/BasicVisualization1/Content/Figures/Gale.png")
+# Plot the figure and save to Unreal Engine's project file (only needed when the random seed is changed above)
+# env.plot("./UE_Projects/BasicVisualization1/Content/Figures/Gale.png")
 
+# Apply actions of Storm, and then record the reward and emission:
 env.seed(seed)
 env.reset()
 actions = deployments_Storm[1:,:] - deployments_Storm[:-1,:]
@@ -143,56 +156,61 @@ while not env.state.is_done():
     observation, reward, _, _ = env.step(actions[env.state.step_count + 1])
 rewards_Storm = np.array(env.state.rewards_all)
 emissions_Storm = np.array(env.state.emission_amount_all)
-# env.plot("U:/Documents/Work/Research/VisualizingRangLEnvInUnrealEngine/UE_Projects/BasicVisualization1/Content/Figures/Storm.png")
+# Plot the figure and save to Unreal Engine's project file (only needed when the random seed is changed above)
+# env.plot("./UE_Projects/BasicVisualization1/Content/Figures/Storm.png")
 
 
+# Since done is fixed to be False, the following will server until the script is killed/closed
 while not done:
-    server.handle_request()
-    if user_policy == 0:
+    server.handle_request() # start to listen the OSC message sent from UMG Widget in Unreal Engine; the received OSC message will change global variables user_policy and user_year
+    if user_policy == 0: # if user selected Breeze:
         print("\nValues sent to OSC server in Unreal Engine for visualization are [step_count, reward, 3-element deployments, CO2 emission amount]: " + str(np.hstack((env.state.step_count, reward, env.state.deployments, env.state.emission_amount))))
-        # env_breeze.plot("U:/Documents/Work/Research/VisualizingRangLEnvInUnrealEngine/UE_Projects/BasicVisualization1/Content/Figures/current_policy.png")
+        # Deprecated: dynamically plotting and saving figures step-by-step to Unreal Engine; no longer needed due to performance issue
+        # env_breeze.plot("./UE_Projects/BasicVisualization1/Content/Figures/current_policy.png")
         client_toUEosc.send_message(
             "/some/address", 
             np.hstack((user_year-2031, 
                        rewards_Breeze[user_year-2031], 
                        deployments_Breeze[1:][user_year-2031], 
                        emissions_Breeze[user_year-2031]))
-            )
-        client_toUMGpopup.send_message(
+            ) # send user specified year's: step_count, Breeze's rewards, deployment numbers, and emission to Unreal Engine for visualization
+        client_toUMGMultiWindow.send_message(
             "/some/address", 
             np.hstack((user_year-2031, 
                        rewards_Breeze[user_year-2031], 
                        deployments_Breeze[1:][user_year-2031], 
                        emissions_Breeze[user_year-2031]))
             )
-    elif user_policy == 1:
+    elif user_policy == 1: # if user selected Gale:
         print("\nValues sent to OSC server in Unreal Engine for visualization are [step_count, reward, 3-element deployments, CO2 emission amount]: " + str(np.hstack((env.state.step_count, reward, env.state.deployments, env.state.emission_amount))))
-        # env_gale.plot("U:/Documents/Work/Research/VisualizingRangLEnvInUnrealEngine/UE_Projects/BasicVisualization1/Content/Figures/current_policy.png")
+        # Deprecated: dynamically plotting and saving figures step-by-step to Unreal Engine; no longer needed due to performance issue
+        # env_gale.plot("./UE_Projects/BasicVisualization1/Content/Figures/current_policy.png")
         client_toUEosc.send_message(
             "/some/address", 
             np.hstack((user_year-2031, 
                        rewards_Gale[user_year-2031], 
                        deployments_Gale[1:][user_year-2031], 
                        emissions_Gale[user_year-2031]))
-            )
-        client_toUMGpopup.send_message(
+            ) # send user specified year's: step_count, Gale's rewards, deployment numbers, and emission to Unreal Engine for visualization
+        client_toUMGMultiWindow.send_message(
             "/some/address", 
             np.hstack((user_year-2031, 
                        rewards_Gale[user_year-2031], 
                        deployments_Gale[1:][user_year-2031], 
                        emissions_Gale[user_year-2031]))
             )
-    elif user_policy == 2:
+    elif user_policy == 2: # if user selected Storm:
         print("\nValues sent to OSC server in Unreal Engine for visualization are [step_count, reward, 3-element deployments, CO2 emission amount]: " + str(np.hstack((env.state.step_count, reward, env.state.deployments, env.state.emission_amount))))
-        # env_storm.plot("U:/Documents/Work/Research/VisualizingRangLEnvInUnrealEngine/UE_Projects/BasicVisualization1/Content/Figures/current_policy.png")
+        # Deprecated: dynamically plotting and saving figures step-by-step to Unreal Engine; no longer needed due to performance issue
+        # env_storm.plot("./UE_Projects/BasicVisualization1/Content/Figures/current_policy.png")
         client_toUEosc.send_message(
             "/some/address", 
             np.hstack((user_year-2031, 
                        rewards_Storm[user_year-2031], 
                        deployments_Storm[1:][user_year-2031], 
                        emissions_Storm[user_year-2031]))
-            )
-        client_toUMGpopup.send_message(
+            ) # send user specified year's: step_count, Storm's rewards, deployment numbers, and emission to Unreal Engine for visualization
+        client_toUMGMultiWindow.send_message(
             "/some/address", 
             np.hstack((user_year-2031, 
                        rewards_Storm[user_year-2031], 
@@ -203,52 +221,52 @@ while not done:
 
 
 
+#%% older implementation for user to manually input years to jump back or forward and apply any one of the 3 scenarios:
+# while not done:
+#     # action = actions[env.state.step_count + 1]
+#     # client.send_message("/agent_action1", np.hstack((done, action, env.state.step_count + 1)))
+#     client.send_message("/agent_action1", np.hstack((done, np.float(user_policy), env.state.step_count)))
+#     server.handle_request()
+#     if user_policy == 0:
+#         actions = deployments_Breeze[1:,:] - deployments_Breeze[:-1,:]
+#     elif user_policy == 1:
+#         actions = deployments_Gale[1:,:] - deployments_Gale[:-1,:]
+#     elif user_policy == 2:
+#         actions = deployments_Storm[1:,:] - deployments_Storm[:-1,:]
+#     if steps_to_apply < 0:
+#         previous_actions = env.state.actions_all[:-abs(steps_to_apply)]
+#         env.seed(seed)
+#         env.reset()
+#         for previous_action in previous_actions:
+#             observation, reward, done, _ = env.step(previous_action)
+#     elif steps_to_apply > 0:
+#         # observation, reward, done, _ = env.step(user_action)
+#         for i in np.arange(steps_to_apply):
+#             observation, reward, done, _ = env.step(actions[env.state.step_count + 1])
+#     # observation, reward, done, _ = env.step(user_action)
+#     print("\nValues sent to OSC server in Unreal Engine for visualization are [step_count, reward, 3-element deployments, CO2 emission amount]: " + str(np.hstack((env.state.step_count, reward, env.state.deployments, env.state.emission_amount))))
+#     env.plot("./UE_Projects/BasicVisualization1/Content/Figures/current_state.png")
+#     client_toUEosc.send_message("/some/address", np.hstack((env.state.step_count, reward, env.state.deployments, env.state.emission_amount)))
+#     client_toUMGpopup.send_message("/some/address", np.hstack((env.state.step_count, reward, env.state.deployments, env.state.emission_amount)))
+#     # client.send_message("/some/address", env.render())
+#     if done:
+#         # client.send_message("/agent_action1", np.hstack((done, action, env.state.step_count)))
+#         client.send_message("/agent_action1", np.hstack((done, np.float(user_policy), env.state.step_count)))
 
-while not done:
-    # action = actions[env.state.step_count + 1]
-    # client.send_message("/agent_action1", np.hstack((done, action, env.state.step_count + 1)))
-    client.send_message("/agent_action1", np.hstack((done, np.float(user_policy), env.state.step_count)))
-    server.handle_request()
-    if user_policy == 0:
-        actions = deployments_Breeze[1:,:] - deployments_Breeze[:-1,:]
-    elif user_policy == 1:
-        actions = deployments_Gale[1:,:] - deployments_Gale[:-1,:]
-    elif user_policy == 2:
-        actions = deployments_Storm[1:,:] - deployments_Storm[:-1,:]
-    if steps_to_apply < 0:
-        previous_actions = env.state.actions_all[:-abs(steps_to_apply)]
-        env.seed(seed)
-        env.reset()
-        for previous_action in previous_actions:
-            observation, reward, done, _ = env.step(previous_action)
-    elif steps_to_apply > 0:
-        # observation, reward, done, _ = env.step(user_action)
-        for i in np.arange(steps_to_apply):
-            observation, reward, done, _ = env.step(actions[env.state.step_count + 1])
-    # observation, reward, done, _ = env.step(user_action)
-    print("\nValues sent to OSC server in Unreal Engine for visualization are [step_count, reward, 3-element deployments, CO2 emission amount]: " + str(np.hstack((env.state.step_count, reward, env.state.deployments, env.state.emission_amount))))
-    env.plot("U:/Documents/Work/Research/VisualizingRangLEnvInUnrealEngine/UE_Projects/BasicVisualization1/Content/Figures/current_state.png")
-    client_toUEosc.send_message("/some/address", np.hstack((env.state.step_count, reward, env.state.deployments, env.state.emission_amount)))
-    client_toUMGpopup.send_message("/some/address", np.hstack((env.state.step_count, reward, env.state.deployments, env.state.emission_amount)))
-    # client.send_message("/some/address", env.render())
-    if done:
-        # client.send_message("/agent_action1", np.hstack((done, action, env.state.step_count)))
-        client.send_message("/agent_action1", np.hstack((done, np.float(user_policy), env.state.step_count)))
 
-
-# def storm_agent(self, seeds):
-#     rewards = []
-#     # deployments = np.array(np.array(pd.read_excel('STORM_Deployments.xlsx'))[-(self.env.param.steps_per_episode+1):,1:],dtype=np.float32)
-#     deployments = np.array(np.array(pd.read_excel('STORM_Deployments_Modified.xlsx'))[-(self.env.param.steps_per_episode+1):,1:],dtype=np.float32)
-#     actions = deployments[1:,:] - deployments[:-1,:]
-#     for seed in seeds:
-#         self.env.seed(seed)
-#         self.env.reset()
-#         while not self.env.state.is_done():
-#             action = actions[self.env.state.step_count + 1]
-#             self.env.step(action)
-#         rewards.append(sum(self.env.state.rewards_all))
-#     return np.mean(rewards)
+# # def storm_agent(self, seeds):
+# #     rewards = []
+# #     # deployments = np.array(np.array(pd.read_excel('STORM_Deployments.xlsx'))[-(self.env.param.steps_per_episode+1):,1:],dtype=np.float32)
+# #     deployments = np.array(np.array(pd.read_excel('STORM_Deployments_Modified.xlsx'))[-(self.env.param.steps_per_episode+1):,1:],dtype=np.float32)
+# #     actions = deployments[1:,:] - deployments[:-1,:]
+# #     for seed in seeds:
+# #         self.env.seed(seed)
+# #         self.env.reset()
+# #         while not self.env.state.is_done():
+# #             action = actions[self.env.state.step_count + 1]
+# #             self.env.step(action)
+# #         rewards.append(sum(self.env.state.rewards_all))
+# #     return np.mean(rewards)
 
 
 
